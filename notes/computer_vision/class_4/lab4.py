b57abc5df0a989e1dc67a1e46bec66fa8d27f343
Patrick Pragman <pat@pragman.io>
2023-09-15 11:15:20 -0800

    yesterday's work

--- notes/computer_vision/class_4/lab4.py
+++ notes/computer_vision/class_4/lab4.py
@@ -15,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     # for consistency
 
 # Create and train a logistic regression model
-lr_model = LogisticRegression(max_iter=1000)
+lr_model = LogisticRegression(max_iter=1000git p)
 lr_model.fit(X_train, y_train)
 
 # Make predictions on the test set
